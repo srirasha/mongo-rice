9a3a1728d4a98fe34c214e2cf496a8811887fd8c
Shahrukh Khan <sha.khan@outlook.fr>
2022-05-29 18:10:57 +0200

    add unit tests

--- tests/MongoRice.Tests/MongoRice.Tests/Repositories/MongoRiceRepositoryTests.cs
+++ tests/MongoRice.Tests/MongoRice.Tests/Repositories/MongoRiceRepositoryTests.cs
@@ -1,6 +1,41 @@
-﻿namespace MongoRice.Tests.Repositories
+﻿using AutoFixture;
+using FluentAssertions;
+using FluentValidation;
+using MongoDB.Bson;
+using MongoRice.Configurations;
+using MongoRice.Documents;
+using MongoRice.Repositories;
+using MongoRice.Tests.TestData;
+using Xunit;
+
+namespace MongoRice.Tests.Repositories
 {
     public class MongoRiceRepositoryTests
     {
+        private readonly Fixture _fixture = new();
+
+        [Fact]
+        public void Instanciation_Should_Throw_ValidationException_when_ConnectionString_IsNullOrEmpty()
+        {
+            IMongoConfiguration mongoConfiguration = _fixture.Build<MongoConfiguration>()
+                                                             .With(prop => prop.ConnectionString, string.Empty)
+                                                             .Create();
+
+            Action constructor = () => { _ = new MongoRiceRepository<MockDocument>(mongoConfiguration); };
+
+            constructor.Should().Throw<ValidationException>("Connection string is empty");
+        }
+
+        [Fact]
+        public void Instanciation_Should_Throw_ValidationException_when_Database_IsNullOrEmpty()
+        {
+            IMongoConfiguration mongoConfiguration = _fixture.Build<MongoConfiguration>()
+                                                             .With(prop => prop.Database, string.Empty)
+                                                             .Create();
+
+            Action constructor = () => { _ = new MongoRiceRepository<MockDocument>(mongoConfiguration); };
+
+            constructor.Should().Throw<ValidationException>("Database is empty");
+        }
     }
 }
